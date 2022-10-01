7e8a145d894b3e5ef59eb025038610829c3809cc
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2022-10-01 13:30:19 +0300

    PCMS_4 : Implement a way for the user to interact with the system

--- src/main/java/com/sda/mierloiubogdan/petclinic/Main.java
+++ src/main/java/com/sda/mierloiubogdan/petclinic/Main.java
@@ -3,12 +3,39 @@ package com.sda.mierloiubogdan.petclinic;
 import com.sda.mierloiubogdan.petclinic.controller.VetController;
 import com.sda.mierloiubogdan.petclinic.repository.VetRepositoryImpl;
 import com.sda.mierloiubogdan.petclinic.service.VetServiceImpl;
+import com.sda.mierloiubogdan.petclinic.utils.SessionManager;
+import com.sda.mierloiubogdan.petclinic.utils.UserOption;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
         VetController vetController = new VetController(new VetServiceImpl(new VetRepositoryImpl()));
-        vetController.createVet();
 
+        Scanner scanner = new Scanner(System.in);
+        UserOption userOption;
+        do {
+            UserOption.displayAllOptions();
+            System.out.println("Please select an option: ");
+            int numbericOption = scanner.nextInt();
+            userOption = UserOption.findByNumericOption(numbericOption);
+            switch (userOption) {
+                case ADD_VET:
+                    vetController.createVet();
+                    break;
+                case VIEW_ALL_VETS:
+                    System.out.println("Not implemented");
+                    break;
+                case UNKNOWN:
+                    System.err.println("Invalid option selected");
+                    break;
+                case EXIT:
+                    System.out.println("Good bye");
+                    break;
 
+            }
+        } while (userOption != UserOption.EXIT);
+        SessionManager.shutdown();
     }
 }
